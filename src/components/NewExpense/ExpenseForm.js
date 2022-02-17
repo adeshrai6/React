15fbb57f7a9f511ec49ee04740e2b328d4b9c6c2
Agya <61568773+adeshrai6@users.noreply.github.com>
2022-02-16 19:53:08 -0500

    getting data from input(ExpenseForm) to App

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import './ExpenseForm.css';
 
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   
 const [enteredTitle, setEnteredTitle] = useState('');
-const [enterdAmount, setEnteredAmount] = useState('');
+const [enteredAmount, setEnteredAmount] = useState('');
 const [enteredDate, setEnteredDate] = useState('');
 
 
@@ -29,26 +29,33 @@ const submitHandler = (event) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enterdAmount,
+      amount: enteredAmount,
       date: new Date(enteredDate)
     }
+
+    props.onSaveExpenseData(expenseData);
+    
+    // Clearing entered field
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
 };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}/>
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
         </div>
 
         <div className="new-expense__control">
             <label>Amount</label>
-            <input type='number'  min="0.01" step="0.01" onChange={amountChangeHandler} />
+            <input type='number' value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
             <label>Date</label>
-            <input type='date' min="2022-02-14" max="2022-12-30" onChange={dateChangeHandler} />
+            <input type='date' value={enteredDate} min="2022-02-14" max="2022-12-30" onChange={dateChangeHandler} />
         </div>
 
         <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,15 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = () => {
+  const saveExpenseDataHendler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData= {saveExpenseDataHendler}/>
     </div>
   );
 };
