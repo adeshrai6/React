8f4bc423a9aa531de006f878c0426a36beff0124
Agya <61568773+adeshrai6@users.noreply.github.com>
2022-02-18 19:00:06 -0500

    implementing props in ExpenseFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,11 @@
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 function Expenses(props) {
   return (
     <Card className="expenses">
+      <ExpensesFilter />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
